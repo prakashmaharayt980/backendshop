4dabaa9b0d76d03fab4b424232605eae5180fe46
prakashmaharayt980 <108126887+prakashmaharayt980@users.noreply.github.com>
2025-04-12 13:25:20 +0545

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -38,25 +38,17 @@ ALLOWED_METHODS = [
     'OPTIONS',
 ]
 
-# CORS settings
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOW_CREDENTIALS = True
-CORS_ALLOWED_ORIGINS = [
-    # 'http://localhost:8080',
-    # 'http://localhost:8001',
-    # 'http://localhost:8080',
-    # 'http://your-production-url.com',
-    # 'http://127.0.0.1',
-    # 'http://127.0.0.1:8000',
-    # 'http://127.0.0.1:8080'
 
+if DEBUG:
+    CORS_ALLOW_ALL_ORIGINS = True
+else:
+    CORS_ALLOW_ALL_ORIGINS = False
+    CORS_ALLOWED_ORIGINS = [
+        'https://shopkeeper-admin.vercel.app',
+        'https://book-heaven-nine.vercel.app',
+        'https://backendshop-production-e1ec.up.railway.app'
+    ]
 
-    'https://shopkeeper-admin.vercel.app',
-'https://book-heaven-nine.vercel.app'
-    
-'https://backendshop-production-e1ec.up.railway.app'
-
-]
 
 # Application definition
 INSTALLED_APPS = [
@@ -83,11 +75,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+      'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 
 
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+  
     
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
