508b553e6733c240cf127b4c0300e9c5700a06cd
prakashmaharayt980 <108126887+prakashmaharayt980@users.noreply.github.com>
2025-04-12 13:35:32 +0545

    oooooo

--- backend/settings.py
+++ backend/settings.py
@@ -39,8 +39,7 @@ ALLOWED_METHODS = [
     'OPTIONS',
 ]
 
-# CORS settings
-CORS_ALLOW_ALL_ORIGINS = True
+
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:8080',
@@ -84,11 +83,12 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
 
 
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+    
     
     'django.middleware.common.CommonMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -189,7 +189,7 @@ REST_FRAMEWORK = {
 
 # SimpleJWT configuration
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=60),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
