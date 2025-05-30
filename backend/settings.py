43fb7d130227e44625ddd0af776a775da9f093c0
thelocalmarket777 <thelocalmarket777@gmail.com>
2025-05-30 10:43:03 +0545

    goood

--- backend/settings.py
+++ backend/settings.py
@@ -26,6 +26,7 @@ ALLOWED_HOSTS = [
    "thelocalmarket.shop",
     "admin-localmarket.vercel.app",
     "backendshop-oy2c.onrender.com",
+    "*"
 
 ]
 
@@ -45,6 +46,7 @@ CORS_ALLOWED_ORIGINS = [
 "https://www.thelocalmarket.shop",
 "https://admin-localmarket.vercel.app",
 "https://backendshop-oy2c.onrender.com",
+"https://thelocalmerket-olive.vercel.app"
 # " http://localhost:8001",
 # " http://localhost:8080",
 
